c7534e1816697fc8dbecec85a7fc206bf6da7099
Vatsalya Anand <vatsalya@buffalo.edu>
2024-03-05 23:26:33 -0500

    Update Portfolio

--- js/script.js
+++ js/script.js
@@ -8,11 +8,3 @@ function toggleMenu() {
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('profile').classList.add('hero-intro');
 });
-
-// Function to toggle between light and dark themes
-function toggleTheme() {
-  document.body.classList.toggle('dark-theme');
-}
-
-// Event listener for theme toggle button
-document.getElementById('theme-toggle').addEventListener('click', toggleTheme);
